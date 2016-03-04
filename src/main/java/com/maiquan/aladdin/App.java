0ce30989fd3b7e2b6ff48659a7bbc5513dbe1ce9
JustArgo <565148812@qq.com>
2016-03-04 14:10:31 +0800

    init aladdin_shopping_cart

---
+++ src/main/java/com/maiquan/aladdin/App.java
@@ -0,0 +1,13 @@
+package com.maiquan.aladdin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
